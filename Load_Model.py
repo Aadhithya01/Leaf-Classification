4db0a1db4c4c903602c4cb16cd49175917350e6f
Aadhithya R <72688656+Aadhithya01@users.noreply.github.com>
2024-03-10 21:30:15 +0530

    Predict Multiple Images from a Directory

--- Load_Model.py
+++ Load_Model.py
@@ -11,15 +11,19 @@ with open('leaf_classification_model.pkl', 'rb') as f:
 with open('class_names.json', 'r') as f:
     class_names = json.load(f)
 
-# Load the image you want to predict
-new_image_path = "C:\\Users\\aadhi\\Desktop\\AV-S-022.jpg"
-new_image = cv2.imread(new_image_path)
-new_image = cv2.resize(new_image, (224, 224))
-new_image = new_image / 255.0  # Normalize pixel values to [0, 1]
-new_image = np.expand_dims(new_image, axis=0)  # Add batch dimension
+image_dir = "C:\\Users\\aadhi\\Documents\\Projects\\Module1\\Training"
 
-# Make predictions
-predictions = model.predict(new_image)
-predicted_class_index = np.argmax(predictions)
-predicted_class_name = class_names[str(predicted_class_index)]
-print(f'Predicted class: {predicted_class_name}')
+for filename in os.listdir(image_dir):
+    if filename.endswith(".jpg") or filename.endswith(".jpeg") or filename.endswith(".png"):
+        image_path = os.path.join(image_dir, filename)
+        new_image = cv2.imread(image_path)
+        new_image = cv2.resize(new_image, (224, 224))
+        new_image = new_image / 255.0  # Normalize pixel values to [0, 1]
+        new_image = np.expand_dims(new_image, axis=0)  # Add batch dimension
+
+        # Make predictions
+        predictions = model.predict(new_image)
+        predicted_class_index = np.argmax(predictions)
+        predicted_class_name = class_names[str(predicted_class_index)]
+        print(f'Predicted class: {predicted_class_name}')
+
